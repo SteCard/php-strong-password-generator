ead1cfe21150390a5f489025f9de86139c868be5
SteCard <stefanocardillo14@gmail.com>
2023-07-05 17:27:05 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Php-password-generator</title>
+</head>
+<body style="background-color: #ababc2">
+    <div class="container my-3">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="fw-bold">Strong Password Generator</h1>
+                <h2 class="text-success fw-bold">Genera una password sicura</h2>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
